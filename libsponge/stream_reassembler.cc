71fa49e1b8f59c7123a315e7e7f183aca8e8b23c
Yaossg <yaossg@outlook.com>
2022-10-14 09:20:31 -0700

    finish lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,5 +1,7 @@
 #include "stream_reassembler.hh"
 
+#include <cstring>
+
 // Dummy implementation of a stream reassembler.
 
 // For Lab 1, please replace with a real implementation that passes the
@@ -12,15 +14,47 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity), _capacity(capacity), buffer(capacity, '\0'), flags(capacity, '\0') {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (eof) {
+        eof_index = index + data.length();
+    }
+    if (!data.empty()) {
+        if (index + data.length() <= cursor) {
+            // do nothing
+        } else if (index < cursor) {
+            push_substring(data.substr(cursor - index), cursor, eof);
+        } else {
+            size_t local = index - cursor;
+            size_t len = min(data.length(), _capacity - local);
+            memcpy(buffer.data() + local, data.data(), len);
+            memset(flags.data() + local, 1, len);
+
+            size_t ones = 0;
+            while (ones < flags.size() && flags.at(ones)) ++ones;
+            size_t written = _output.write(buffer.substr(0, ones));
+            cursor += written;
+
+            memmove(buffer.data(), buffer.data() + written, buffer.size() - written);
+            memmove(flags.data(), flags.data() + written, flags.size() - written);
+            memset(flags.data() + flags.size() - written, 0, written);
+        }
+
+    }
+    if (_output.bytes_written() == eof_index) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    size_t result = 0;
+    for (char flag : flags) if (flag) ++result;
+    return result;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return unassembled_bytes() == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,8 @@
 
 #include <cstdint>
 #include <string>
+#include <map>
+#include <queue>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +16,10 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    std::string buffer;
+    std::string flags;
+    size_t cursor{};
+    size_t eof_index = -1;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
